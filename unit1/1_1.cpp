57ac4b28874da099581054ef6db809fade032c37
HelioStudio <842770903@qq.com>
2021-01-20 23:52:24 +0800

    Modify a word

--- unit1/1_1.cpp
+++ unit1/1_1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     string name;
-    cout<<"please entern your name\n";
+    cout<<"please enter your name\n";
     cin>>name;
     cout<<"hello, "<<name<<"... and goodbye";
     system("Pause");
